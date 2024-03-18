18647771cde539f2594d8e206c26dbdabed8230c
Will Metzler <metzlerwill@gmail.com>
2024-03-18 14:54:37 -0400

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+const record = [
+  { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"},
+];
+
+
+function superbowlWin(record) {
+    for (const season of record) {
+        if (season.result === "W") {
+            return season.year;
+        }
+    }
+};
+
+record.find(superbowlWin);
+
